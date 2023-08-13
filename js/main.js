0488cc0b3457b7ddebbedcae0a9197677ce655ed
Alina <alinkartemenko@mail.ru>
2023-08-13 18:29:43 +0300

    added adaptability

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-$(function(){
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
 
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("menu--active");
 });
